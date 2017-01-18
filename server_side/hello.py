77a37e82db1976a02b6319aaa01d49c4149332f7
ophirharpaz <ophirharpaz@gmail.com>
2017-01-18 12:00:12 +0200

    settings popup, graphics and server files

--- server_side/hello.py
+++ server_side/hello.py
@@ -1,6 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 	
+	
+@app.route('/')
+def intro():
+	return 'Hello world!'
+	
+	
 @app.route('/user/<username>')
 def show_user_profile(username):
     return 'Hello, %s!' % (username, )

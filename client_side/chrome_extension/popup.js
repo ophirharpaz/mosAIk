09da14a9cda880a028cc1f5cffb54853093f1a0e
unknown <kim.belassen@gmail.com>
2017-01-18 17:23:29 +0200

    JS and css changes

--- client_side/chrome_extension/popup.js
+++ client_side/chrome_extension/popup.js
@@ -2,13 +2,30 @@ $(document).ready(function() {
 
 	function sendRequest() {
 		var nbWindows = document.getElementById("max_windows").value;
+		var ids = new Array();
+		var urls = new Array();
+		var listIdsUrls = new Array(ids,urls);
+		
+		var queryInfo = {
+			currentWindow: true
+		};
+		chrome.tabs.query(queryInfo, function(tabs) {
+			tabs.forEach(function(tab) {
+				listIdsUrls.push(tab.id,tab.url);
+			});
+		});
+		console.log(listIdsUrls);
 		var sent_data = { nbWindows: nbWindows};
 
 		$.ajax({
 			url: 'http://192.168.0.139:5000/user/kim',
-			type: 'GET',
-			data: sent_data,
-			contentType: 'application/json; charset=utf-8',
+			type: 'POST',
+			contentType: 'application/json',
+			data: JSON.stringify({
+	    		nbWindows: nbWindows,
+	    		aa: listIdsUrls
+			}),
+			dataType: 'json',
 			success: function (response) {
 				alert(response);
 			},
@@ -19,79 +36,7 @@ $(document).ready(function() {
 	}
 
 	document.getElementById("split_button").addEventListener("click", function(){
-    	sendRequest();
+		sendRequest();
 	});
 
 });
-// $(document).ready(function() {
-//     // Lorsque je soumets le formulaire
-//     $('#save_settings').on('submit', function(e) {
-//         e.preventDefault(); // J'empêche le comportement par défaut du navigateur, c-à-d de soumettre le formulaire
-
-//         var $this = $(this); // L'objet jQuery du formulaire
-
-//         // Je récupère les valeurs
-//         var max_tabs_per_window = $('#max_tabs_per_window').val();
-//         var max_windows = $('#max_windows').val();
-//  		console.log(max_tabs_per_window);
-//         // Je vérifie une première fois pour ne pas lancer la requête HTTP
-//         // si je sais que mon PHP renverra une erreur
-//         if(max_tabs_per_window === '' || max_windows === '') {
-//             alert('Les champs doivent êtres remplis');
-//         } else {
-//             // Envoi de la requête HTTP en mode asynchrone
-//             $.ajax({
-//                 url: $this.attr('action'), // Le nom du fichier indiqué dans le formulaire
-//                 type: $this.attr('method'), // La méthode indiquée dans le formulaire (get ou post)
-//                 data: $this.serialize(), // Je sérialise les données (j'envoie toutes les valeurs présentes dans le formulaire)
-//                 success: function(html) { // Je récupère la réponse du fichier PHP
-//                     alert(html); // J'affiche cette réponse
-//                 }
-//             });
-//         }
-//     });
-// });
-
-// function getAllTabsUrls() {
-
-//   var queryInfo = {
-//     currentWindow: true
-//   };
-
-//   chrome.tabs.query(queryInfo, function(tabs) {
-//     tabs.forEach(function(tab) {
-//         console.log('Tab URL: ', tab.url);
-//         console.log('Tab Title: ', tab.title);
-// 		console.log('Tab ID: ', tab.id);
-// 		chrome.tabs.executeScript(tab.id, { "code": "document.documentElement.outerHTML;" }, function (result) {
-// 			console.log(result);
-// 		});
-//     });
-//   });
-
-// };
-
-// function getAllTabsHtmls() {
-// 	var queryInfo = {
-// 		active: true
-// 	};
-
-// 	chrome.tabs.query(queryInfo, function(tabs) {
-// 		var tab = tabs[0];
-// 		var tabId = tab.id;
-// 		chrome.tabs.executeScript(tabId, { "code": "document.documentElement.outerHTML;" }, function (result) {
-// 			console.log(result);
-// 		});
-// 	});
-// };
-
-// function renderStatus(statusText) {
-//   document.getElementById('status').textContent = statusText;
-// };
-
-
-// document.addEventListener('DOMContentLoaded', function() {
-// 	console.log("Hello");
-// 	getAllTabsUrls();
-// 	// getAllTabsHtmls();
-// });
